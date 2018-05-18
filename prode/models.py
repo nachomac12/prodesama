5d9c85c7e38cf9fe05e34c0c9dc9818bceddc40a
luis-sama <luisuran.bit@gmail.com>
2018-05-18 14:41:56 -0300

    Carga de los equipos

--- prode/models.py
+++ prode/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Team(models.Model):
     name = models.CharField(max_length=50)
     flag = models.ImageField()
+    score = models.IntegerField(default=0)
     def __str__(self):
         return self.name
 
