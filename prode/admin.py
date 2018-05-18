dabdd45539e43940db4427c0b900b94e902b1fa7
luis-sama <luisuran.bit@gmail.com>
2018-05-18 18:34:15 -0300

    Agrego los modelos para partido y apuestas

--- prode/admin.py
+++ prode/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Team
+from .models import Team, Match, Bet
 
 
 admin.site.register(Team)
+admin.site.register(Match)
+admin.site.register(Bet)
--- prode/models.py
+++ prode/models.py
@@ -7,10 +7,16 @@ class Team(models.Model):
     def __str__(self):
         return self.name
 
-# class Match(models.Model):
-#     start = models.DateTimeField()
-#     end = models.DateTimeField()
-#     team1 = models.ForeignKey(Team, on_delete=models.CASCADE)
-    # team2 = models.ForeignKey(Team, on_delete=models.CASCADE)
-    # team1_score = models.PositiveIntegerField()
-    # team2_score = models.PositiveIntegerField()
+class Match(models.Model):
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    team1 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='+')
+    team2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='+')
+    team1_score = models.PositiveIntegerField(default=0)
+    team2_score = models.PositiveIntegerField(default=0)
+    print(str(team1))
+
+class Bet(models.Model):
+    match = models.ForeignKey(Match, on_delete=models.CASCADE, related_name='+')
+    team1_score = models.PositiveIntegerField(default=0)
+    team2_score = models.PositiveIntegerField(default=0)
