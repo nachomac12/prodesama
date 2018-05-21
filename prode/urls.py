338f715714083d1eabb8bf566d927fd89c4bade6
luis-sama <luisuran.bit@gmail.com>
2018-05-21 08:47:19 -0300

    Agrego el template para el login de usuario

--- prode/urls.py
+++ prode/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'prode'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('login/', auth_views.login, {'template_name': 'prode/login.html'} )
 ] 
