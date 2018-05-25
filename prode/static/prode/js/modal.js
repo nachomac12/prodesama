0059eadf6a123d64c95a67a0cb6dffaf121ba059
luis-sama <luisuran.bit@gmail.com>
2018-05-24 23:30:39 -0300

    Login en el modal

--- prode/static/prode/js/modal.js
+++ prode/static/prode/js/modal.js
@@ -11,20 +11,21 @@ const registerBody = document.getElementById('register-body');
 function chooseModal(option) {
   modal.classList.add('is-active');
   if (option=='register') {  //sign up
-            signInTitle.classList.add('is-hidden'); 
-            loginButton.classList.add('is-hidden');
-            loginBody.classList.add('is-hidden');
-            signUpTitle.classList.remove('is-hidden');
-            registerButton.classList.remove('is-hidden');
-            registerBody.classList.remove('is-hidden');
-            }   else {  //sign in
-            signInTitle.classList.remove('is-hidden'); 
-            loginButton.classList.remove('is-hidden');
-            loginBody.classList.remove('is-hidden');
-            signUpTitle.classList.add('is-hidden');
-            registerButton.classList.add('is-hidden');
-            registerBody.classList.add('is-hidden');
-        } 
+    signInTitle.classList.add('is-hidden'); 
+    loginButton.classList.add('is-hidden');
+    loginBody.classList.add('is-hidden');
+    signUpTitle.classList.remove('is-hidden');
+    registerButton.classList.remove('is-hidden');
+    registerBody.classList.remove('is-hidden');
+    }   
+  else {  //sign in
+    signInTitle.classList.remove('is-hidden'); 
+    loginButton.classList.remove('is-hidden');
+    loginBody.classList.remove('is-hidden');
+    signUpTitle.classList.add('is-hidden');
+    registerButton.classList.add('is-hidden');
+    registerBody.classList.add('is-hidden');
+  }
 }
 
 //Cierra el modal
