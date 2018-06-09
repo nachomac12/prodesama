9a2be4734823c5e4ac32dbb2a8686781d5a1852d
nachomac12 <macias.ignacio@hotmail.com>
2018-06-09 00:55:32 -0300

    Agrego clase UserProfile

--- prode/models.py
+++ prode/models.py
@@ -15,17 +15,26 @@ class Match(models.Model):
     team2 = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='+')
     team1_score = models.PositiveIntegerField(default=0)
     team2_score = models.PositiveIntegerField(default=0)
+    end_flag = models.BooleanField(default=False)
+
     def __str__(self):
         return (self.team1.name + " vs " + self.team2.name)
         
 class Bet(models.Model):
-    user = models.ForeignKey(User, related_name='bets', blank=True, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='bets', blank=True, on_delete=models.CASCADE)
     match = models.ForeignKey(Match, on_delete=models.CASCADE, related_name='bets') 
     team1_score = models.PositiveIntegerField(default=0)
     team2_score = models.PositiveIntegerField(default=0)
+    result = models.PositiveIntegerField(default=0, null=True)
 
     class Meta:
         unique_together = ('user', 'match')
 
     def __str__(self):
         return (str(self.match))
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    global_score = models.PositiveIntegerField(default=0)
+    global_ranking = models.PositiveIntegerField(default=0)
+
