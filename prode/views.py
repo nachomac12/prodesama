0cc7d15e3a5a6d4664341ab6f9bc1feabb84434d
nachomac12 <macias.ignacio@hotmail.com>
2018-06-20 21:21:35 -0300

    push

--- prode/views.py
+++ prode/views.py
@@ -17,8 +17,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return all existing match on database."""
-        #return Match.objects.filter(competition=1).filter(end__gte=timezone.now()).order_by('start')
-        return Match.objects.filter(competition__available=True).filter(end__gte=timezone.now()).order_by('start')
+        return Match.objects.filter(competition__available=True).filter(end__gte=timezone.now()).order_by('start')[:10]
 
 def signup(request):
     if request.method == 'POST':
