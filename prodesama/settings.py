49915dd50c25df4916f00de74b909d1a4aa636bc
luis-sama <luisuran.bit@gmail.com>
2018-07-03 00:29:12 -0300

    añado el host a settings

--- prodesama/settings.py
+++ prodesama/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = '7!5&@b4#+m7p+=lz($lo8$7@86@!$r&6_jn+xcnipqj-vw7)eu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'prodesama.herokuapp.com'
+]
 
 
 # Application definition
